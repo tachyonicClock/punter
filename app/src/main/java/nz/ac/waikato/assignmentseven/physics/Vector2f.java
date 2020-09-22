5cceb736013de2ecac0415f092a4c954dd7b9c2a
Anton Lee <anton.j.15.le@gmail.com>
2020-09-22 16:47:09 +1200

    Add Vector2f logic

--- app/src/main/java/nz/ac/waikato/assignmentseven/physics/Vector2f.java
+++ app/src/main/java/nz/ac/waikato/assignmentseven/physics/Vector2f.java
@@ -1,4 +1,60 @@
 package nz.ac.waikato.assignmentseven.physics;
 
+/**
+ * Vector2 is a 2D vector.
+ * It is often used to represent velocity, position, or acceleration
+ */
 public class Vector2f {
+    public float x;
+    public float y;
+
+    public Vector2f invert(){
+        return new Vector2f(-x, -y);
+    }
+
+    public Vector2f multiply(float s){
+        return new Vector2f(x * s, y * s);
+    }
+
+    public Vector2f add(Vector2f p2){
+        return new Vector2f(x + p2.x, y + p2.y);
+    }
+
+    public Vector2f subtract(Vector2f p2){
+        return new Vector2f(x - p2.x, y - p2.y);
+
+    }
+
+    public Vector2f normalized(){
+        float m = magnitude();
+        if (m == 0) return new Vector2f();
+        return new Vector2f(x/m, y/m);
+    }
+
+    public float magnitude(){
+        return (float) Math.sqrt(x*x + y*y);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public Vector2f(Vector2f position) {
+        x = position.x;
+        y = position.y;
+    }
+
+    public Vector2f(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2f(){
+        this.x = 0;
+        this.y = 0;
+    }
 }
