bf1557333283e90c1bd1e3a64f041607099a3f1f
Anton Lee <anton.j.15.le@gmail.com>
2020-09-22 16:45:32 +1200

    Add GameObject

---
+++ app/src/main/java/nz/ac/waikato/assignmentseven/physics/Transform.java
@@ -0,0 +1,7 @@
+package nz.ac.waikato.assignmentseven.physics;
+
+public class Transform {
+    Vector2f translation;
+    Vector2f scale;
+    float rotation;
+}
---
+++ app/src/main/java/nz/ac/waikato/assignmentseven/physics/Vector2f.java
@@ -0,0 +1,4 @@
+package nz.ac.waikato.assignmentseven.physics;
+
+public class Vector2f {
+}
